a7a2d3c2606525312b1d7cb52446492d5f00b530
victor hugo <victoralmeidadev.io@gmail.com>
2024-10-20 23:53:38 -0300

    ajustado tag a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { imagetools } from 'vite-imagetools'
+import viteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(imagetools)],
+  plugins: [react(imagetools,viteCompression)],
 })
